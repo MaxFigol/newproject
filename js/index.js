8dbaa60e3d288b549a3aaf2d32615e9a6d977c6d
Max <maksfigol@gmail.com>
2022-03-24 19:54:00 +0200

    -afor

--- js/index.js
+++ js/index.js
@@ -10,12 +10,22 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+// let a = prompt('Один из просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+for(let i = 0; i < 2; i++){
+    let a = prompt('Один из просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+}
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+if (numberOfFilms < 10) {
+    console.log("Вы мало посмотрели фильмов");
+} else {
+    console.log("Вы киноман");
+}
 console.log(personalMovieDB);
---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+let num = 10;
+num == 11 ? console.log(`Hello`) : console.log(`World`);
+
+let number = 20;
+
+while(number < 25) {
+    console.log(number)
+    number++
+};
+
+for(let i = 0; i < 10; i++){
+    console.log(i)
+};

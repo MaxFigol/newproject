3c2dae7cae81ae6d5512ab257d113a7761c02f92
Max <maksfigol@gmail.com>
2022-03-27 20:00:32 +0300

    newjs

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ while(number < 25) {
 for(let i = 0; i < 10; i++){
     console.log(i);
 }
+
+let arr = [2, 34, 12, 7];
+arr.sort(function(a, b){
+    return a - b;
+});
+
+console.log(arr);
+
+const obj = {
+    a: 23,
+    b: 34,
+    c: "new"
+};
+
+const newObj = {...obj};
+newObj.a = 43;
+console.log(newObj);

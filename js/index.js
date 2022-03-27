f0a85a2cd327baf0161cdd19464df822b2579ecf
Max <maksfigol@gmail.com>
2022-03-27 17:19:59 +0300

    -anew

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько вы фильмов посмотрели?', '');
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt('Сколько вы фильмов посмотрели?', '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько вы фильмов посмотрели?', '');
+    }
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,22 +18,41 @@ const personalMovieDB = {
     privat: false
 };
 
-// let a = prompt('Один из просмотренных фильмов?', ''),
-//     b = prompt('На сколько оцените его?', ''),
-//     c = prompt('Один из просмотренных фильмов?', ''),
-//     d = prompt('На сколько оцените его?', '');
 
+function question(){
+    for(let i = 0; i < 2; i++){
+        let a = prompt('Один из просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+        personalMovieDB.movies[a] = b;
+    }
+}
 
-for(let i = 0; i < 2; i++){
-    let a = prompt('Один из просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
+question();
+
+function ratingProfile (){
+    if (numberOfFilms < 10) {
+        console.log("Вы мало посмотрели фильмов");
+    } else {
+        console.log("Вы киноман");
+    }
 }
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-if (numberOfFilms < 10) {
-    console.log("Вы мало посмотрели фильмов");
-} else {
-    console.log("Вы киноман");
+
+ratingProfile();
+
+function showMyDB(hidden){
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
 }
-console.log(personalMovieDB);
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for(let i = 0; i<=2; i++){
+        const genre = prompt(`Ващ любимый жанр под номером ${i + 1}`);
+        personalMovieDB.genres[i] = genre;
+    }
+}
+
+writeYourGenres();
+
--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ num == 11 ? console.log(`Hello`) : console.log(`World`);
 let number = 20;
 
 while(number < 25) {
-    console.log(number)
-    number++
-};
+    console.log(number);
+    number++;
+}
 
 for(let i = 0; i < 10; i++){
-    console.log(i)
-};
+    console.log(i);
+}
